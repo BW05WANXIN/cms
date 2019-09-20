26a0d47c51d61e37d4e825e343bdc1058c5cfdae
BW05WANXIN <2293152624@qq.com>
2019-09-20 10:09:35 +0800

    第二次

---
+++ src/main/java/com/wanxin/entity/VoteStatic.java
@@ -0,0 +1,43 @@
+package com.wanxin.entity;
+
+import com.alibaba.druid.sql.visitor.functions.Char;
+
+/**
+ * @author 926474
+ *投票统计实体类
+ */
+public class VoteStatic {
+	
+	//
+	String optionKey;
+	Integer voteNum;
+	String optionTitle;
+	Integer voteNumTotal;
+	
+	public Integer getVoteNumTotal() {
+		return voteNumTotal;
+	}
+	public void setVoteNumTotal(Integer voteNumTotal) {
+		this.voteNumTotal = voteNumTotal;
+	}
+	public String getOptionKey() {
+		return optionKey;
+	}
+	public void setOptionKey(String optionKey) {
+		this.optionKey = optionKey;
+	}
+	public Integer getVoteNum() {
+		return voteNum;
+	}
+	public void setVoteNum(Integer voteNum) {
+		this.voteNum = voteNum;
+	}
+	public String getOptionTitle() {
+		return optionTitle;
+	}
+	public void setOptionTitle(String optionTitle) {
+		this.optionTitle = optionTitle;
+	}
+	
+	
+}
